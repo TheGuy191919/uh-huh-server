f4541040050ecc8640b5975a27ba2b5b9c7714b2
TheGuy191919 <evan_1998@sina.com>
2014-12-20 21:29:40 -0500

    Implemented name hash for rooms

--- src/main/java/io/github/theguy191919/server/api/rooms/ChatRoom.java
+++ src/main/java/io/github/theguy191919/server/api/rooms/ChatRoom.java
@@ -5,10 +5,38 @@
  */
 package io.github.theguy191919.server.api.rooms;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author evan__000
  */
-public class ChatRoom {
+public class ChatRoom implements Runnable{
+    private int nameHash = (int)(Math.random() * 10000);
+    private List<>
+
+    public ChatRoom(String name){
+        this.nameHash = name.hashCode();
+    }
     
+    public void start(){
+        
+    }
+    
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public void stop(){
+        
+    }
+
+    /**
+     * @return the nameHash
+     */
+    public int getNameHash() {
+        return nameHash;
+    }
 }
